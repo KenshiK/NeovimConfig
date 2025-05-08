0aa1ff22dd018aeb011248264ce4732edfad10fd
KenshiK <mbakim@hotmail.fr>
2025-05-08 11:45:17 +0200

    Lua line complete

--- lua/plugins/mini-statusline.lua
+++ lua/plugins/mini-statusline.lua
@@ -3,20 +3,25 @@ return
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local custom_onedark = require'lualine.themes.onedark'
+
+        -- Change the background of lualine_c section for normal mode
+        custom_onedark.normal.c.bg = '#232326'
         require('lualine').setup({
-            options = { theme = 'onedark' },
+            options = {
+                    theme = custom_onedark, --'onedark',
+                    component_separators = '',
+                    section_separators = { left = '', right = '' },
+                },
             sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { {'filetype', icon = { align = 'left' }}, 'lsp_status'},
+                lualine_a = { {'mode', separator = { left = '' }, right_padding = 2 } },
+                lualine_b = { {'filetype', icon = { align = 'left' }}, 'lsp_status',  },
                 lualine_c = { {'filename', path = 1}, 'diagnostics' },
-                lualine_x = { 'diff', 'branch' },
-                lualine_y = { 'encoding', 'fileformat', 'progress' },
-                lualine_z = { 'location' }
+                lualine_x = {  },
+                lualine_y = { 'diff', 'branch', 'fileformat', 'encoding' },
+                lualine_z = { {'location', separator = { right = '' }, left_padding = 2 } }
             },
-            extensions = { 
-                    'lazy', 'mason',
-                    -- 'nvim-dap-ui', 'fzf' 
-                }
+            extensions = { 'lazy', 'mason', 'nvim-dap-ui', 'fzf' }
         })
     end
 }
